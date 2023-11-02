2b941a9028113496a5d97ec8a1da129a764f4813
Mchtsz <mhatland206@gmail.com>
2023-11-02 13:46:44 +0100

    Update user CRUD operations in app.js

--- app.js
+++ app.js
@@ -8,20 +8,29 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(publicMappe));
 
+/* 
 const insertStmt = db.prepare(
   `INSERT INTO users (name, email, password) VALUES (?, ?, ?);`
 );
 
-insertStmt.run("John Doe", "jh@gmail.com", "123456");
+const updateStmt = db.prepare(
+  `UPDATE users SET name = ?, email = ?, password = ? WHERE id = ?;`
+);
+
+const removeStmt = db.prepare(`DELETE FROM users WHERE id = ?;`); 
+
+ insertStmt.run("John Doe", "jh@gmail.com", "123456");
+
+*/
 
 app.post("/addUser", (req, res) => {
-    const { name, email, password } = req.body;
-    const insertStmt = db.prepare(
-        `INSERT INTO users (name, email, password) VALUES (?, ?, ?);`
-    );
-    insertStmt.run(name, email, password);
-    res.send("User added successfully");
-})
+  const { name, email, password } = req.body;
+  const insertStmt = db.prepare(
+    `INSERT INTO users (name, email, password) VALUES (?, ?, ?);`
+  );
+  insertStmt.run(name, email, password);
+  res.send("User added successfully");
+});
 
 app.listen(3000, () => {
   console.log(`Server running on port 3000`);
